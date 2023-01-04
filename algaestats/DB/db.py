8ffa6d5dfad817dce8226b094ad206a639ea01dc
Neko Juers <neko.juers@bloomington.in.gov>
2023-01-04 14:54:41 -0500

    Update requirements.txt.

--- algaestats/DB/db.py
+++ algaestats/DB/db.py
@@ -4,17 +4,18 @@
 import sqlalchemy as sqa
 import pandas as pd
 
+
 class FlowCamConnection:
     def __init__(
         self,
         local_db=None,
         flowcam_db="mariadb+mariadbconnector://readonly:cobreadonly@10.111.100.9/flowdb?charset=utf8mb4",
-        ):
+    ):
         """
-            Instantiates connection to readonly flowcam database.
-            
-            local_db (str): The URL of the local database to connect to.
-            flowcam_db (str): The URL of the flowcam read-only db to connect to.
+        Instantiates connection to readonly flowcam database.
+
+        local_db (str): The URL of the local database to connect to.
+        flowcam_db (str): The URL of the flowcam read-only db to connect to.
         """
         self.engines = {
             "flowdb": sqa.create_engine(flowcam_db),
@@ -24,19 +25,21 @@ class FlowCamConnection:
             "flowdb": sqa.MetaData(),
             # "localdb": sqa.MetaData(),
         }
-        return None 
+        return None
 
     def _query(self, query=None):
         self._connect()
         particle_property = sqa.Table(
-                    "particle_property",
-                    self.metadata["flowdb"],
-                    autoload=True,
-                    autoload_with=self.engines["flowdb"],
-                )
+            "particle_property",
+            self.metadata["flowdb"],
+            autoload=True,
+            autoload_with=self.engines["flowdb"],
+        )
         if not query:
             # get first ten particles
-            query = sqa.select([particle_property]).where(particle_property.columns.particle <=10)
+            query = sqa.select([particle_property]).where(
+                particle_property.columns.particle <= 10
+            )
         out = pd.DataFrame(self.connections["flowdb"].execute(query).fetchall())
         self._disconnect()
         return out
@@ -53,6 +56,6 @@ class FlowCamConnection:
             engine.dispose()
         return None
 
+
 if __name__ == "__main__":
     c = FlowCamConnection()
-    breakpoint()

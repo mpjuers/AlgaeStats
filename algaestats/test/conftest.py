edbeaca7426a7f28f07603d1208f996094fc0f67
Neko Juers <neko.juers@bloomington.in.gov>
2023-01-04 14:18:04 -0500

    First test works!

--- algaestats/test/conftest.py
+++ algaestats/test/conftest.py
@@ -5,14 +5,14 @@ from pickle import load
 
 import pytest as pt
 
-import algaestats as as
+import algaestats.db.db as adb
 
 @pt.fixture
 def fixture_query_null():
     """
     Default query parameters.
     """
-    with open("./TestAssest/test_query_1.pickle", "rb") as file:
+    with open("./test/TestAssets/test_query_1.pickle", "rb") as file:
         return load(file)
 
 @pt.fixture
@@ -20,3 +20,4 @@ def fixture_connection():
     """
     
     """
+    return adb.FlowCamConnection()
--- algaestats/test/test_query.py
+++ algaestats/test/test_query.py
@@ -2,3 +2,6 @@
 # Copyright 2023 Neko Juers
 
 import pytest as pt
+
+def test_query_null(fixture_query_null, fixture_connection):
+    assert fixture_connection._query().equals(fixture_query_null)

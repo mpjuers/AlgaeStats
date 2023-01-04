7419086cd9d132f210f708617ce8f0f008fc773c
Neko Juers <neko.juers@bloomington.in.gov>
2023-01-04 15:52:56 -0500

    Added local database.

--- algaestats/DB/db.py
+++ algaestats/DB/db.py
@@ -8,7 +8,7 @@ import pandas as pd
 class FlowCamConnection:
     def __init__(
         self,
-        local_db=None,
+        local_db="sqlite:///localdb.db",
         flowcam_db="mariadb+mariadbconnector://readonly:cobreadonly@10.111.100.9/flowdb?charset=utf8mb4",
     ):
         """
@@ -19,12 +19,23 @@ class FlowCamConnection:
         """
         self.engines = {
             "flowdb": sqa.create_engine(flowcam_db),
-            # "localdb": sqa.create_engine(local_db),
+            "localdb": sqa.create_engine(local_db),
         }
         self.metadata = {
             "flowdb": sqa.MetaData(),
-            # "localdb": sqa.MetaData(),
+            "localdb": sqa.MetaData(),
         }
+        return None
+
+    def _create_table(self):
+        tab = sqa.Table(
+            "table",
+            self.metadata["localdb"],
+            sqa.Column("id", sqa.Integer, primary_key=True),
+            sqa.Column("default_classification", sqa.Integer),
+            sqa.Column("operator_classification", sqa.Integer),
+        )
+        self.metadata["localdb"].create_all(self.engines["localdb"])
         return None
 
     def _query(self, query=None):
@@ -46,8 +57,8 @@ class FlowCamConnection:
 
     def _connect(self):
         self.connections = {
-            "flowdb": self.engines["flowdb"].connect()
-            # "localdb": self.engines["localdb"].connect()
+            "flowdb": self.engines["flowdb"].connect(),
+            "localdb": self.engines["localdb"].connect(),
         }
         return None
 
@@ -59,3 +70,4 @@ class FlowCamConnection:
 
 if __name__ == "__main__":
     c = FlowCamConnection()
+    breakpoint()
